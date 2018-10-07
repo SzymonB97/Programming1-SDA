702772cc72daf02f13592f69b764305f2e428f80
Szymon Borowy <szymonborowy@wp.pl>
2018-10-07 14:43:28 +0200

    Change in method

--- src/main/java/core/algorithms/PrimeNumber.java
+++ src/main/java/core/algorithms/PrimeNumber.java
@@ -19,14 +19,12 @@ class PrimeNumber {
     private static boolean checkIfPrime(int number) {
         int numberOfDivisors = 0;
 
-        for (int i = 1; i <= number; i++) {
+        for (int i = 2; i < number; i++) {
             if (number % i == 0) {
-                numberOfDivisors ++;
+                return false;
             }
         }
 
-        if (number == 1) {
-            return true;
-        } else return numberOfDivisors == 2;
+        return true;
     }
 }
