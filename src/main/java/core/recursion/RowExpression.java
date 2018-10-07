b85bf7a1a95d4797bf430d479ce67d08c91e68e5
Szymon Borowy <szymonborowy@wp.pl>
2018-10-07 15:55:44 +0200

    Expression of row

---
+++ src/main/java/core/recursion/RowExpression.java
@@ -0,0 +1,21 @@
+package core.recursion;
+
+import java.util.Scanner;
+
+class RowExpression {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj liczbę");
+        double index = scanner.nextInt();
+
+        System.out.println("Wynik: " + row(index));
+    }
+
+    private static double row(double n) {
+        if (n == 1) return 1;
+        if (n == 2) return 0.5;
+
+        return -(row(n-1)) * row(n-2);
+    }
+}
