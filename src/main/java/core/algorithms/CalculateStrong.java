23aa2cb25aea7a2edc195080c6acb482eef71d38
Szymon Borowy <szymonborowy@wp.pl>
2018-10-07 11:36:25 +0200

    Calculate strong

---
+++ src/main/java/core/algorithms/CalculateStrong.java
@@ -0,0 +1,29 @@
+/*Program obliczający silnię podanej liczby*/
+
+package core.algorithms;
+
+import java.util.Scanner;
+
+class CalculateStrong {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj liczbę");
+        int number = scanner.nextInt();
+
+        System.out.println("Silnia podanej liczby wynosi: " + calculate(number));
+    }
+
+    private static int calculate(int number) {
+        int result = 1;
+
+        if (number == 0) {
+            return result;
+        }
+        else for (int i = 2; i <= number; i++) {
+            result = result * i;
+        }
+
+        return result;
+    }
+}
