f8d57704f04a30c2f6bc638505f6e91f85b6c7ec
Szymon Borowy <szymonborowy@wp.pl>
2018-10-13 13:48:19 +0200

    Calculate sum of next expressions of row

---
+++ src/main/java/core/algorithms/AmountRow/AmountCalculator.java
@@ -0,0 +1,28 @@
+package core.algorithms.AmountRow;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+class AmountCalculator {
+
+    final static String FILE_PATH = "D:\\Projekty Java\\programming1\\src\\main\\java\\core\\algorithms\\AmountRow\\data";
+
+    public static void main(String[] args) {
+        calculate();
+    }
+
+    private static void calculate() {
+
+        int sum = 0;
+        try (Scanner scanner = new Scanner(new File(FILE_PATH))) {
+            while (scanner.hasNextInt()) {
+                int digit = scanner.nextInt();
+                sum += digit;
+                System.out.println(sum);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
