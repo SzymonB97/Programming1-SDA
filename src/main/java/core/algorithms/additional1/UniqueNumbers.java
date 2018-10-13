8448f807ea2c7cb11331d7a4e960be5b8f45e783
Szymon Borowy <szymonborowy@wp.pl>
2018-10-13 14:29:30 +0200

    Change to sum of numbers without repeat

--- src/main/java/core/algorithms/additional1/UniqueNumbers.java
+++ src/main/java/core/algorithms/additional1/UniqueNumbers.java
@@ -1,7 +1,7 @@
 /*
-Zad dodatkowe 1.Mamy dużo liczb od 1 do 10. Zliczamy ile jest niepowtarzających liczb (zlożoność liniowa)
+Zad dodatkowe 1.Mamy dużo liczb od 1 do 10. Zliczamy ile jest różnych liczb (zlożoność liniowa)
 
-        np. 1 2 6 7 3 2 3 4 6 8 5 3 2 1 1 1 1 1 1 2 2 3 5 6 5 5 5 5 5 5 5 5 5 1
+np. 1 2 6 7 3 2 3 4 6 8 5 3 2 1 1 1 1 1 1 2 2 3 5 6 5 5 5 5 5 5 5 5 5 1
 */
 
 package core.algorithms.additional1;
@@ -23,7 +23,7 @@ class UniqueNumbers {
     private static void calculate() {
 
         //zerujemy tablicę, każde zero odpowiada za jedną liczbę i zlicza jej wystąpienia
-        //na tej podstawie wyznacza unikalne liczby
+        //na tej podstawie wyznacza ilość liczb (nie wliczając powtórzeń)
         int[] amount = new int[10];
         int unique = 0;
         Arrays.fill(amount, 0);
@@ -34,11 +34,11 @@ class UniqueNumbers {
             }
 
             for (int i = 0; i < amount.length; i++) {
-                if (amount[i] == 1) unique++;
+                if (amount[i] != 0) unique++;
             }
 
             System.out.println("Wystąpienia liczb: " + Arrays.toString(amount));
-            System.out.println("Ilość unikalnych liczb: " + unique);
+            System.out.println("Ilość liczb: " + unique);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
